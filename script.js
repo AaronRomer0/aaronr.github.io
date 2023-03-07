d14eb56d3f73181d2c18e580ca95b462b2ed4d61
AaronRomer0 <122638688+AaronRomer0@users.noreply.github.com>
2023-03-06 22:53:20 -0300

    initial comit

---
+++ script.js
@@ -0,0 +1,3 @@
+function hola(){
+    document.write("Otros Conocimientos: Inglés: Nivel Intermedio. Portugués: Nivel Intermedio. avanzado de Office; Microsoft Word.  Microsoft PowerPoint. Microsoft Excel.Base de Datos: MySQL. ")
+}
